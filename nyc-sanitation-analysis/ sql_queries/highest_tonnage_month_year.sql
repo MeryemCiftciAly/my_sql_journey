34b05b6e9e97a0dc956528211568d188d1ea0bbb
MeryemCiftciAly <merygiwork3@gmail.com>
2025-07-20 10:25:31 -0400

    Add files via upload

---
+++ nyc-sanitation-analysis/ sql_queries/highest_tonnage_month_year.sql
@@ -0,0 +1,10 @@
+-- Which brough highest paper tonnage in a single month and year?
+SELECT
+	borough,
+	month,
+	SUM(paper_tons_collected) AS Total_Paper_collected
+FROM cleaned_sanitation_data
+GROUP BY 
+	borough, 
+	month
+ORDER BY Total_Paper_collected DESC;
