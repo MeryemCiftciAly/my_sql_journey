8c44821ed178b6eecf5881df3eb85dcb2c98df5a
MeryemCiftciAly <merygiwork3@gmail.com>
2025-07-20 17:35:50 -0400

    Update highest_organic_waste.sql

--- nyc-sanitation-analysis/ sql_queries/highest_organic_waste.sql
+++ nyc-sanitation-analysis/ sql_queries/highest_organic_waste.sql
@@ -7,13 +7,12 @@ SELECT * FROM cleaned_sanitation_data;
 
 WITH organic_waste_total AS ( 
  	SELECT
-	    borough, 
-		SUBSTRING(month FROM 8 FOR 2) AS month_number,
+	    borough, REPLACE(month, '/', '-') AS year_month,
 		SUM(res_organics_tons +
 		school_organic_tons +
 		leaves_organic_tons) AS organics
 FROM cleaned_sanitation_data
-GROUP BY borough, month
+GROUP BY year_month, borough
 )
 SELECT * FROM organic_waste_total
 ORDER BY organics DESC
