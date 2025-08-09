c5569904d318eb2143358620f5a8cc47125f1900
MeryemCiftciAly <merygiwork3@gmail.com>
2025-08-09 14:23:19 -0400

    Update highest_tonnage_month_year.sql

--- nyc-sanitation-analysis/03_sql_scripts/highest_tonnage_month_year.sql
+++ nyc-sanitation-analysis/03_sql_scripts/highest_tonnage_month_year.sql
@@ -1,1 +1,10 @@
-placeholder
+--Question: Which brough had the highest paper tonnage in a single month and year?
+SELECT
+	borough,
+	month,
+	SUM(paper_tons_collected) AS Total_Paper_collected
+FROM cleaned_sanitation_data
+GROUP BY 
+	borough, 
+	month
+ORDER BY Total_Paper_collected DESC;
