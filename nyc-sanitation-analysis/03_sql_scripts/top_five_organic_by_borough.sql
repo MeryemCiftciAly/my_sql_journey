07f069e83e47727a2bc303f43ce141e70aa0602e
MeryemCiftciAly <merygiwork3@gmail.com>
2025-08-09 14:30:30 -0400

    Update top_five_organic_by_borough.sql

--- nyc-sanitation-analysis/03_sql_scripts/top_five_organic_by_borough.sql
+++ nyc-sanitation-analysis/03_sql_scripts/top_five_organic_by_borough.sql
@@ -1,1 +1,15 @@
-placeholders
+--Question: Top 5 boroughs with the most orgaic collection overall?
+
+SELECT * FROM  cleaned_sanitation_data;
+
+WITH top_five_by_borough AS(
+	SELECT
+		borough,
+		SUM(
+		res_organics_tons +
+		school_organic_tons  +
+		leaves_organic_tons
+		) AS total_organic
+ FROM cleaned_sanitation_data
+GROUP BY borough, month
+)
