7c8789e98b3920cec8701101274e113b0410d1b3
MeryemCiftciAly <merygiwork3@gmail.com>
2025-08-14 02:36:08 +0300

    Add mew SQL practice files from DataWithBaraa

---
+++ sql_practice/ 00_basics/select.sql
@@ -0,0 +1,12 @@
+--Selecting all data
+SELECT * FROM customers;
+
+--retrieving all order data
+SELECT * FROM orders;
+
+--selecting specific columns
+SELECT
+	first_name,
+	country,
+	score
+FROM customers;
