f3db0a98b834e20b4d17c91308407f16b688c7b7
MeryemCiftciAly <merygiwork3@gmail.com>
2025-09-02 09:39:01 -0400

    Update data_quality_checks.sql

--- 07_data_warehouse/medallion_architecture/01_project/scripts/gold_schema/data_quality_checks.sql
+++ 07_data_warehouse/medallion_architecture/01_project/scripts/gold_schema/data_quality_checks.sql
@@ -1,106 +1,57 @@
-
-
-USE datawarehouse;
-
--- Checking if silver layer has product number with space
-SELECT prd_nm
-FROM datawarehouse.silver_crm_prd_info
-WHERE prd_nm != TRIM(prd_nm);
-
--- Checking for duplicate data if there are duplicate customer IDs
-SELECT cust_id,
-COUNT(*)
-FROM datawarehouse.silver_crm_cst_inf0
-GROUP BY cust_id
-HAVING COUNT(*) > 1;
-
--- Checking for null or negative numbers
-SELECT prd_cost
-FROM datawarehouse.silver_crm_prd_info
-WHERE prd_cost < 0 OR prd_cost IS NULL;
-
--- ======================================================================================
--- Checks for invalid start date structure
--- The date checks id the start date is greater than the end date
--- The date checks if the sales order date is greater than the ship or delivery date
--- The date checks if the order date is in the future or in the past
+-- =======================================================================================
+-- Gold Layer Data Quality & Integrity Checks
+-- 1. Validate referential integrity by checking for missing customer/product keys.  
+-- 2. Identify duplicate customer IDs that may cause data inconsistencies.  
+-- 3. Ensure proper data integration by prioritizing CRM gender data when available.  
+-- 4. Verify that tables can be correctly joined for accurate reporting.  
 -- =======================================================================================
 
-SELECT * FROM datawarehouse.silver_crm_prd_info
-WHERE prd_start_dt > prd_end_dt;
+-- Checking for null customer and product keys from different tables
+SELECT *
+FROM datawarehouse.gold_facts_sales AS f
+LEFT JOIN datawarehouse.gold_dim_customers AS c
+ON c.customer_key = f.product_key
+LEFT JOIN datawarehouse.gold_dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL
 
-SELECT * FROM datawarehouse.silver_crm_sales_details
-WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt;
-
-SELECT
-NULLIF(sls_order_dt, 0) sls_order_dt
-FROM datawarehouse.silver_crm_sales_details
-WHERE sls_order_dt <= 0
-OR LENGTH(sls_order_dt)!= 8
-OR sls_order_dt > 20500101
-OR sls_order_dt > 19000101;
-
--- Checking monetary value structure
-SELECT DISTINCT
-sls_sales,
-sls_quantity,
-sls_price
-FROM datawarehouse.silver_crm_sales_details
-WHERE sls_sales != sls_quantity * sls_price
-OR sls_sales IS NULL OR sls_quantity IS NULL OR sls_price IS NULL
-OR sls_sales <= 0 OR sls_quantity <= 0 OR sls_price <= 0
-ORDER BY sls_sales, sls_quantity, sls_price;
-
--- Handling low cardinality data
-SELECT DISTINCT
-gen,
-CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN ('Female')
-     WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN ('Male')
-ELSE 'n/a'
-END gen
-FROM datawarehouse.bronze_erp_cust_az12;
-
-SELECT 
-CASE WHEN TRIM(cntry) = 'DE' THEN 'GERMANY'
-     WHEN TRIM(cntry) IN ('US', 'USA') THEN  'United States'
-     WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
-ELSE TRIM(cntry)
-END AS cntry
-FROM datawarehouse.bronze_erp_cust_az212;
-
--- Separating product ID
-SELECT
-CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid))
-     ELSE cid
-END cid,
-bdate,
-gen
-FROM datawarehouse.bronze_erp_cust_az12
-WHERE bdate < '1920-01-01' OR bdate > NOW();
-
--- Checking the prouct key length
-SELECT prd_key, LENGTH(prd_key)
-FROM datawarehouse.silver_crm_prd_info
-ORDER BY LENGTH(prd_key) DESC
-LIMIT 10;
-
--- Checking the ID between the silver and bronze layer
-SELECT
-id, 
-cat, 
-subcat, 
-maintenance
-FROM datawarehouse.bronze_erp_px_cat_g1v2 WHERE id NOT IN 
-(SELECT cat_id FROM datawarehouse.silver_crm_prd_info);
--- returns one ID that is not in the silver layer
-
--- Fixing overlapping date in the dataset
-SELECT
-prd_id,
-prd_key,
-prd_nm,
-prd_start_dt,
-prd_end_dt,
-LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL 1 DAY AS prd_end_dt
-FROM datawarehouse.bronze_crm_prd_info
-WHERE prd_key IN ('AC-HE-HL-U509-R', 'AC-HE-HL-U509');
+  
+-- identify duplicate cust_id values
+SELECT cust_id, COUNT(*) 
+	FROM (Select 
+		ci.cust_id, 
+		ci.cust_key, 
+		ci.cust_firstname, 
+		ci.cust_lastname, 
+		ci.cst_marital_status, 
+		ci.cst_gndr, 
+		ci.cst_create_date,
+		ca.bdate,
+		ca.gen,
+		la.cntry
+	From datawarehouse.silver_crm_cst_inf0 as ci
+	LEFT JOIN datawarehouse.silver_erp_cust_az12 as ca
+	ON ci.cust_key = ca.cid
+	LEFT JOIN datawarehouse.silver_erp_loc_a101 as la
+	ON ci.cust_key =la.cid)t
+    GROUP BY cust_id
+    HAVING COUNT(*) > 1;
+    
+     
+     -- =======================================================================================
+    -- Data integration
+    -- The CASE statement ensures that ci.cst_gndr (CRM data) is prioritized if it's not 'n/a'
+	-- If ci.cst_gndr is 'n/a', it falls back to ca.gen, or 'n/a' if both are null
+    -- ==========================================================================================
+Select DISTINCT
+		ci.cst_gndr, 
+		ca.gen,
+        CASE WHEN  ci.cst_gndr != 'n/a' THEN  ci.cst_gndr -- CRM is the Master for the gender data
+	ELSE COALESCE(ca.gen, 'n/a')
+end as new_gen
+	From datawarehouse.silver_crm_cst_inf0 as ci
+	LEFT JOIN datawarehouse.silver_erp_cust_az12 as ca
+	ON ci.cust_key = ca.cid
+	LEFT JOIN datawarehouse.silver_erp_loc_a101 as la
+	ON ci.cust_key =la.cid
+    GROUP BY ci.cst_gndr, ca.gen;
