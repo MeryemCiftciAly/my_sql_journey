eaf4de583ce13feebefc150ee17de6e9fe0c59e3
MeryemCiftciAly <merygiwork3@gmail.com>
2025-12-14 13:55:55 -0500

    Update and rename 01_intermediate_having_1.sql to 01_interm_having_orderby_1.sql

---
+++ 01_sql_practice/01_intermediate/03_queries_reporting/01_interm_having_orderby_1.sql
@@ -0,0 +1,22 @@
+--Select all the authors that write more than an average of 150,000 words per book. 
+
+SELECT 
+  author, AVG(words) AS avg_words
+FROM 
+  books
+GROUP BY 
+  author
+HAVING
+  AVG(words) > 150000
+=====================================================================================
+--Find the top 3 authors by book count.
+SELECT 
+  author, 
+  COUNT(id) AS total_books
+FROM 
+  books
+GROUP BY
+  author
+ORDER BY 
+  total_books DESC
+lIMIT 3;
