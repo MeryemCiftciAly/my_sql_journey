cde4aede42519d418783f576ec497fb06fedc4dc
MeryemCiftciAly <merygiwork3@gmail.com>
2025-12-15 15:38:41 -0500

    Update candy_sales.sql

--- 02_mini-projects/lina_christmas_project/01_ddl_schema_definition/candy_sales.sql
+++ 02_mini-projects/lina_christmas_project/01_ddl_schema_definition/candy_sales.sql
@@ -1,2 +1,8 @@
-
-EMPTY
+CREATE TABLE candy_sales (
+	sale_id INTEGER PRIMARY KEY,
+	flavor TEXT NOT NULL,
+	neighborhood TEXT NOT NULL,
+	quantity_sold INTEGER NOT NULL,
+	price_per_unit DECIMAL (10, 2) NOT NULL,
+	revenue DECIMAL(10, 2) GENERATED ALWAYS AS (quanity_sold * price_per_unit) STORED
+ );
