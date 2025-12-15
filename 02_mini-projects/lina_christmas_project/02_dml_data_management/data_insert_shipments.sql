f9bd84fdf33b4a29d632b657f9cfcfba110a2934
MeryemCiftciAly <merygiwork3@gmail.com>
2025-12-15 17:26:50 -0500

    Create data_insert_shipments.sql

---
+++ 02_mini-projects/lina_christmas_project/02_dml_data_management/data_insert_shipments.sql
@@ -0,0 +1,10 @@
+INSERT INTO shipments (shipment_id, gift_id, country, shipping_cost, delivery_status, expected_delivery_date, actual_delivery_date) VALUES
+(201, 101, 'US', 8.00, 'On Time', '2024-12-24', '2024-12-22'),
+(202, 102, 'US', 8.00, 'On Time', '2024-12-24', '2024-12-23'),
+(203, 103, 'Canada', 18.00, 'Delayed', '2024-12-24', '2024-12-26'),
+(204, 104, 'Canada', 18.00, 'On Time', '2024-12-24', '2024-12-21'),
+(205, 105, 'US', 8.00, 'Delayed', '2024-12-24', '2024-12-27'), 
+(206, 106, 'US', 8.00, 'On Time', '2024-12-24', '2024-12-24'),
+(207, 107, 'Jamaica', 25.00, 'On Time', '2024-12-24', '2024-12-23'),
+(208, 108, 'Brazil', 22.00, 'In Transit', '2024-12-24', NULL), 
+(209, 109, 'US', 8.00, 'On Time', '2024-12-24', '2024-12-22');
