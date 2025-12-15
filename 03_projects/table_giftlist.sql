39ecc41cf4325077ed02b6532f824c0b61e3d353
MeryemCiftciAly <merygiwork3@gmail.com>
2025-12-15 15:47:02 -0500

    Delete 03_projects/table_giftlist.sql

--- 03_projects/table_giftlist.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE giftlist (
-    gift_id INTEGER PRIMARY KEY,
-    relative_name TEXT NOT NULL,
-    relationship TEXT NOT NULL,
-    gift_purchased TEXT NOT NULL,
-    purchase_price DECIMAL(10, 2) NOT NULL
-);
