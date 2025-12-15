1f53bda7735bd4dc409dc0cf0cafe64c90736b57
MeryemCiftciAly <merygiwork3@gmail.com>
2025-12-15 17:24:13 -0500

    Create insert_data_gift_list.sql

---
+++ 02_mini-projects/lina_christmas_project/02_dml_data_management/insert_data_gift_list.sql
@@ -0,0 +1,10 @@
+INSERT INTO gift_list (gift_id, relative_name, relationship, gift_purchased, purchase_price) VALUES
+(101, 'Mother', 'Mother', 'Silk Scarf', 65.00),     
+(102, 'Robert', 'Father', 'Leather Wallet', 50.00),   
+(103, 'Mateo', 'Brother', 'Book Set', 35.00),       
+(104, 'Janelle', 'Sister-in-law', 'Art Supplies', 40.00), 
+(105, 'Sara', 'Sister', 'Gaming Headset', 75.00),    
+(106, 'David', 'Uncle', 'Coffee Sampler', 25.00),   
+(107, 'Rosa', 'Aunt', 'Handmade Jewelry', 45.00),     
+(108, 'Carol', 'Aunt', 'E-Reader', 80.00),          
+(109, 'Daniel', 'Cousin', 'Board Game', 50.00);
