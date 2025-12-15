21d3f8a74f345ca1e7cf8614b5405d6741eac706
MeryemCiftciAly <merygiwork3@gmail.com>
2025-12-15 17:21:26 -0500

    Update table_candy_sales.sql

--- 02_mini-projects/lina_christmas_project/01_ddl_schema_definition/table_candy_sales.sql
+++ 02_mini-projects/lina_christmas_project/01_ddl_schema_definition/table_candy_sales.sql
@@ -6,5 +6,5 @@ CREATE TABLE candy_sales (
 	neighborhood TEXT NOT NULL,
 	quantity_sold INTEGER NOT NULL,
 	price_per_unit DECIMAL (10, 2) NOT NULL,
-	revenue DECIMAL(10, 2) GENERATED ALWAYS AS (quanity_sold * price_per_unit) STORED
+	revenue DECIMAL(10, 2) GENERATED ALWAYS AS (quanity_sold * price_per_unit) STORED -- This is a calculated field that will auto populate the revenue when code is executed.
  );
