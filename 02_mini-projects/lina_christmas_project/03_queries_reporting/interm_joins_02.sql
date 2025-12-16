bb7f914f0b3c66581ad43547a91bc6e10d8143e8
MeryemCiftciAly <merygiwork3@gmail.com>
2025-12-16 15:48:31 -0500

    Update interm_joins_02.sql

--- 02_mini-projects/lina_christmas_project/03_queries_reporting/interm_joins_02.sql
+++ 02_mini-projects/lina_christmas_project/03_queries_reporting/interm_joins_02.sql
@@ -9,3 +9,11 @@
 After further thought and feedback the question was clarified.
 "Which shipments" should be interpreted as returning the details of the gift and the recipient.*/
 
+SELECT DISTINCT
+	 g.relative_name,
+	 g.gift_purchased, 
+ 	s.delivery_status  
+FROM 
+	 shipments s
+JOIN gift_list g ON s.gift_id = g.gift_id 
+WHERE delivery_status= "Delayed"
