2e84e4fc9c9dffe42332c139eff2164bbe7d5a68
MeryemCiftciAly <merygiwork3@gmail.com>
2025-12-16 15:22:10 -0500

    Create interm_joins_02.sql

---
+++ 02_mini-projects/lina_christmas_project/03_queries_reporting/interm_joins_02.sql
@@ -0,0 +1,11 @@
+--=====================================================================================
+--Which shipments were officially marked as 'Delayed' and missed the December 24 deadline? 
+--====================================================================================
+
+/* Here "which shipments." presentes some ambiguity. It is uncertain 
+ --The specific item (gift_purchased)?
+ --The recipient (relative_name)?
+ --The destination (country)?
+After further thought and feedback the question was clarified.
+"Which shipments" should be interpreted as returning the details of the gift and the recipient.*/
+
