11cd5660c572dc86ad9ae2f6068ab8b08f237660
MeryemCiftciAly <merygiwork3@gmail.com>
2026-01-11 23:32:35 +0300

    Add project scope table for NY poverty-unemployment project

---
+++ 03_projects/poverty-unemployment-analysis/01_ddl_schema_definition/03_bronze_project_scopedata.sql
@@ -0,0 +1,30 @@
+
+--==========================================================================
+--Project Scope Table Creation: New York Poverty Data
+--Purpose: First step before beginning the silver layer
+--Extract all New York data from the bronze layer
+--==========================================================================
+
+--Creating table with only New York data for the scope of the project from the bronze layer
+
+CREATE TABLE bronze.bronze_source_poverty_ny AS
+SELECT *
+FROM bronze.poverty_fact
+WHERE stateabbrv = 'NY';
+
+--Inspecting table after creation
+
+SELECT *
+FROM bronze_source_poverty_ny
+LIMIT 100;
+
+--Checking bronze schema for table creation
+
+SELECT 
+	table_name,
+	column_name,
+	data_type
+FROM information_schema.columns
+WHERE table_schema ='bronze'
+ORDER BY table_name, ordinal_position;
+
