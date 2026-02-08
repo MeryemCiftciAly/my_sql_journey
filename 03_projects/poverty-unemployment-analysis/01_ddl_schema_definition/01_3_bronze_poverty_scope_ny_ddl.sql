95cdb24c343db6a4b59d8bf47aa7f967e75b26f1
MeryemCiftciAly <merygiwork3@gmail.com>
2026-02-08 11:51:20 -0500

    Update 01_3_bronze_poverty_scope_ny_ddl.sql

--- 03_projects/poverty-unemployment-analysis/01_ddl_schema_definition/01_3_bronze_poverty_scope_ny_ddl.sql
+++ 03_projects/poverty-unemployment-analysis/01_ddl_schema_definition/01_3_bronze_poverty_scope_ny_ddl.sql
@@ -1,14 +1,9 @@
 
 /*==========================================================================
  Poverty Dataset - Bronze Layer
-
 Project Scope Table Creation: New York Poverty Data
-
-Purpose: First step before beginning the silver layer
 Extract all New York data from the bronze layer
 --==========================================================================*/
-
---Creating table with only New York data for the scope of the project from the bronze layer
 
 CREATE TABLE bronze.bronze_source_poverty_ny AS
 SELECT *
@@ -20,14 +15,4 @@ WHERE stateabbrv = 'NY';
 SELECT *
 FROM bronze_source_poverty_ny
 LIMIT 100;
-
---Checking bronze schema for table creation
-
-SELECT 
-	table_name,
-	column_name,
-	data_type
-FROM information_schema.columns
-WHERE table_schema ='bronze'
-ORDER BY table_name, ordinal_position;
 
