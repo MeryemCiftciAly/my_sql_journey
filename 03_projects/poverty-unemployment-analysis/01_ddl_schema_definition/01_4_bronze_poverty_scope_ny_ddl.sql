c0fb4100fecdbe11efffc46a6e06e8cc2989e8f9
MeryemCiftciAly <merygiwork3@gmail.com>
2026-02-08 18:44:43 -0500

    Update 01_4_bronze_poverty_scope_ny_ddl.sql

--- 03_projects/poverty-unemployment-analysis/01_ddl_schema_definition/01_4_bronze_poverty_scope_ny_ddl.sql
+++ 03_projects/poverty-unemployment-analysis/01_ddl_schema_definition/01_4_bronze_poverty_scope_ny_ddl.sql
@@ -5,7 +5,7 @@ Project Scope Table Creation: New York Poverty Data
 Extract all New York data from the bronze layer
 --==========================================================================*/
 
-CREATE TABLE bronze.bronze_source_poverty_ny AS
+CREATE TABLE bronze.bronze_source_poverty_ny 
 SELECT *
 FROM bronze.poverty_fact
 WHERE stateabbrv = 'NY';
