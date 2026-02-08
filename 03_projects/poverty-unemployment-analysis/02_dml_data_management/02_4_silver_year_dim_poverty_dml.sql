5ff6fede14710731a34a5507ca93a8962e052a06
MeryemCiftciAly <merygiwork3@gmail.com>
2026-02-08 16:06:40 -0500

    Update 02_4_silver_year_dim_poverty_dml.sql

--- 03_projects/poverty-unemployment-analysis/02_dml_data_management/02_4_silver_year_dim_poverty_dml.sql
+++ 03_projects/poverty-unemployment-analysis/02_dml_data_management/02_4_silver_year_dim_poverty_dml.sql
@@ -9,7 +9,6 @@ WITH cleaned_attributes_year AS(
 	SELECT
 		CAST(regexp_replace(attribute, '.*_(\d{4})$', '\1') AS INTEGER) AS year_extracted
 	FROM bronze_source_poverty_ny
-	WHERE attribute_value ~ '^\d+(\.\d+)?$'
 )
 INSERT INTO silver.year_dim_poverty(
 SELECT DISTINCT year_extracted AS year
