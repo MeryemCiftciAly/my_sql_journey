8f4ee673267dbad69523d2966f97962cb98c2ca6
MeryemCiftciAly <merygiwork3@gmail.com>
2026-02-08 14:44:58 -0500

    Update 02_1_silver_income_fact_poverty_ddl.sql

--- 03_projects/poverty-unemployment-analysis/01_ddl_schema_definition/02_1_silver_income_fact_poverty_ddl.sql
+++ 03_projects/poverty-unemployment-analysis/01_ddl_schema_definition/02_1_silver_income_fact_poverty_ddl.sql
@@ -4,7 +4,7 @@ Poverty Dataset
 Creatıng Silver Layer: Income Fact Table DDL
 --=========================================*/
 
-CREATE TABLE IF NOT EXISTS silver.income_fact(
+CREATE TABLE IF NOT EXISTS silver.income_fact_poverty(
 	fips_code INTEGER NOT NULL,
 	year INTEGER NOT NULL,
 	stateabbrev CHAR(2) NOT NULL,
@@ -16,13 +16,8 @@ CREATE TABLE IF NOT EXISTS silver.income_fact(
 	PRIMARY KEY (fips_code, year, attribute)
 );
 
-CREATE INDEX indx_income_fact_fips_county_year ON silver.income_fact(fips_code, county, year);
-CREATE INDEX  indx_income_fact_year ON silver.income_fact(year);
+CREATE INDEX indx_income_fact_fips_county_year ON silver.income_fact_poverty(fips_code, county, year);
+CREATE INDEX  indx_income_fact_year ON silver.income_fact_poverty(year);
 
-COMMENT ON TABLE silver.income_fact IS 'Standardize and transformed data from bronze layer includes median, upper and lower income'
+COMMENT ON TABLE silver.income_fact_poverty IS 'Standardize and transformed data from bronze layer includes median, upper and lower income';
 
-ALTER TABLE silver.income_fact
-RENAME TO income_fact_poverty;
-
-COMMENT ON TABLE silver.income_fact_poverty IS
-'Standardize and transformed data from bronze layer includes median, upper and lower income';
