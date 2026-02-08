965dadcd92931b83830c8045b01c2b259f14784e
MeryemCiftciAly <merygiwork3@gmail.com>
2026-02-08 11:50:07 -0500

    Update 01_4_bronze_laborforce_scope_ny_ddl.sql

--- 03_projects/poverty-unemployment-analysis/01_ddl_schema_definition/01_4_bronze_laborforce_scope_ny_ddl.sql
+++ 03_projects/poverty-unemployment-analysis/01_ddl_schema_definition/01_4_bronze_laborforce_scope_ny_ddl.sql
@@ -1,3 +1,12 @@
+
+/*==========================================================================
+Laborforce Dataset - Bronze Layer
+
+Project Scope Table Creation: New York laborforce Data
+
+Purpose: Extract all New York data from the bronze layer
+--==========================================================================*/
+
 SET search_path TO bronze;
 
 CREATE TABLE bronze.source_laborforce_ny AS
