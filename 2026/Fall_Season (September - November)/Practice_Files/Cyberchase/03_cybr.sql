b50e4c55d741bce8cfa3b7ffd3b6bb38dc462350
MeryemCiftciAly <merygiwork3@gmail.com>
2026-09-24 13:21:34 -0400

    Create 03_cybr.sql

---
+++ 2026/Fall_Season (September - November)/Practice_Files/Cyberchase/03_cybr.sql
@@ -0,0 +1,11 @@
+--Find the production code for the episode “Hackerized!”.
+
+SELECT production_code
+FROM episodes
+WHERE title = 'Hackerized!';
+
++-----------------+
+| production_code |
++-----------------+
+| CYB091          |
++-----------------+
