d708cb46df2e29df76c9d00e983590eb8be00d79
MeryemCiftciAly <merygiwork3@gmail.com>
2026-09-24 13:08:31 -0400

    Create 01_cybr.sql

---
+++ 2026/Fall_Season (September - November)/Practice_Files/Cyberchase/01_cybr.sql
@@ -0,0 +1,38 @@
+--Write a query to list the titles of all episodes in Cyberchase’s original season, Season 1.
+
+SELECT title
+FROM episodes
+WHERE season = 1;
+
++--------------------+
+
++-------------------------------------+
+|                title                |
++-------------------------------------+
+| Lost My Marbles                     |
+| Castleblanca                        |
+| R-Fair City                         |
+| Snow Day to be Exact                |
+| Sensible Flats                      |
+| Zeus on the Loose                   |
+| The Poddleville Case                |
+| And They Counted Happily Ever After |
+| Clock Like An Egyptian              |
+| Secrets of Symmetria                |
+| A Day at the Spa                    |
+| Of All The Luck                     |
+| Eureeka                             |
+| Cool It                             |
+| Find Those Gleamers                 |
+| Codename: Icky                      |
+| Return to Sensible Flats            |
+| Problem Solving in Shangri-La       |
+| Send in the Clones                  |
+| Trading Places                      |
+| Less Than Zero                      |
+| Model Behavior                      |
+| Fortress of Attitude                |
+| Size Me Up                          |
+| A Battle of Equals                  |
+| Out of Sync                         |
++-------------------------------------+
